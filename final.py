7dd8baf6b74b499d0b39537464c2621466911646
Vincent_NTUT_LAB1321 <ssuhsien@usc.edu>
2023-04-25 17:31:48 -0700

    improve the effi. of split algo., try to summerize to final.py

--- final.py
+++ final.py
@@ -6,30 +6,13 @@ import numpy as np
 import math
 from split_algorithm import get_splits
 
-def normalize_vector(vector):
-    """Normalize a given vector to a unit vector."""
-    norm = math.sqrt(sum(i**2 for i in vector))
-    return [i / norm for i in vector]
-
-
-def cosine_similarity(vector1, vector2):
-    dot_product = sum(p*q for p,q in zip(vector1, vector2))
-    magnitude1 = math.sqrt(sum([(val)**2 for val in vector1]))
-    magnitude2 = math.sqrt(sum([val**2 for val in vector2]))
-    return dot_product / (magnitude1 * magnitude2)
-
-def euclidean_distance(vector1, vector2):
-    distance = 0
-    for i in range(len(vector1)):
-        distance += (vector1[i] - vector2[i]) ** 2
-    return math.sqrt(distance)
 
 # Load the video file
 cap = cv2.VideoCapture('InputVideo.mp4')
 
 total_frame = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-sample_rate = int(total_frame * 2e-3)
+sample_rate = max(int(total_frame * 2e-3), 1)
 
 prev_frame = None
 last_shot = 0
@@ -47,9 +30,21 @@ for i in range(num_frames):
     if not ret:
         break
 
+
     frame_num = cap.get(cv2.CAP_PROP_POS_FRAMES);
 
     if prev_frame is not None:
+
+        frame_diff = cv2.absdiff(frame, prev_frame)
+        
+        th = 15
+
+        if cv2.mean(frame_diff)[0] > th and cv2.mean(frame_diff)[1] > th and cv2.mean(frame_diff)[2] > th:
+            if frame_num - last_shot > 20:
+                shot_frame_index.append(frame_num)
+                print("scene change! at %s"%(frame_num)) 
+                print("mean diff in each channel are: R: %.1f G: %.1f B: %.1f" %(cv2.mean(frame_diff)[2], cv2.mean(frame_diff)[1], cv2.mean(frame_diff)[0]))
+            last_shot = frame_num
 
         (means, stds) = cv2.meanStdDev(frame)
         frame_v = np.concatenate([means, stds]).flatten()
@@ -58,23 +53,8 @@ for i in range(num_frames):
             frames[0].append(frame_v)
             frames[1].append(frame_num)
 
-    
-        frame_hist = cv2.calcHist([frame], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-        frame_hist = frame_hist.flatten()
-        (means_prev, stds_prev) = cv2.meanStdDev(prev_frame)
-        prev_frame_v = np.concatenate([means_prev, stds_prev]).flatten()
-        prev_frame_hist = cv2.calcHist([prev_frame], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-        prev_frame_hist = prev_frame_hist.flatten()
-        cos_sim = cosine_similarity(frame_hist, prev_frame_hist)
-        e_dist_hist = euclidean_distance(frame_hist, prev_frame_hist)
-        e_dist_v = euclidean_distance(frame_v, prev_frame_v)
-
-        if ((e_dist_hist > 10000 and e_dist_v >= 10) or e_dist_v > 20):
-            if frame_num - last_shot > 10:
-                shot_frame_index.append(frame_num)
-                print("shot change dectected, the euclidean_distance is (%.4f, %4f), and the frame index is %f" %(e_dist_hist, e_dist_v, frame_num))
-            last_shot = frame_num
     prev_frame = frame
+        
 
 errors = []
 choices = dict()
@@ -91,7 +71,7 @@ for i in range(len(scenes)):
             if j == len(shot_frame_index) - 1:
                 scenes[i] = shot_frame_index[j]
             elif shot_frame_index[j + 1] > scenes[i]:
-                if (shot_frame_index[j + 1] - scenes[i]) > 200 and (scenes[i] - shot_frame_index[j]) > 200:
+                if min((shot_frame_index[j + 1] - scenes[i]),(scenes[i] - shot_frame_index[j])) > 200:
                     break
                 if scenes[i] - shot_frame_index[j] <= shot_frame_index[j + 1] - scenes[i]:
                     scenes[i] = shot_frame_index[j]
