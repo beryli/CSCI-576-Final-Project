1ff7f458aedad12ed0e6e6f5aed191ceb4ac47e9
Nick Chen <haojungc@gmail.com>
2023-04-28 17:29:09 -0700

    Add argument parser
    usage: main.py [-h] -vi <input-video> -ai <input-audio> [-vo <output-video>]
                   [-ao <output-audio>]
    
    optional arguments:
      -h, --help            show this help message and exit
      -vi <input-video>, --input-video <input-video>
      -ai <input-audio>, --input-audio <input-audio>
      -vo <output-video>, --output-video <output-video>
      -ao <output-audio>, --output-audio <output-audio>

--- src/main.py
+++ src/main.py
@@ -1,22 +1,22 @@
-import sys
+import argparse
 import converter
-from frame import Frametype
 from video import analyze_video
 
 if __name__ == '__main__':
-    if len(sys.argv) < 5:
-        print("Missing Arguments", file=sys.stderr)
-        print("Sample Usage:", file=sys.stderr)
-        print(
-            "\t$ python %s <input-video>.rgb <output-video>.avi <input-audio>.wav <output-audio>.mp3"
-            % (sys.argv[0]), file=sys.stderr
-        )
-        sys.exit()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-vi', '--input-video',
+                        required=True, metavar='<input-video>', dest='v_in')
+    parser.add_argument('-ai', '--input-audio',
+                        required=True, metavar='<input-audio>', dest='a_in')
+    parser.add_argument('-vo', '--output-video',
+                        default='output.avi', metavar='<output-video>', dest='v_out')
+    parser.add_argument('-ao', '--output-audio',
+                        default='output.mp3', metavar='<output-audio>', dest='a_out')
+    args = parser.parse_args()
 
-    [v_in, v_out, a_in, a_out] = sys.argv[1:5]
-    converter.rgb2avi(v_in, v_out)
-    converter.wav2mp3(a_in, a_out)
+    converter.rgb2avi(args.v_in, args.v_out)
+    converter.wav2mp3(args.a_in, args.a_out)
     
-    frames = analyze_video(v_out)
+    frames = analyze_video(args.v_out)
     print("[Frames]")
     print(frames)
