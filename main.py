c9b83ea0bdc6a48a22e0097e5b4d0bdb9f4cf65a
Nick Chen <haojungc@gmail.com>
2023-05-03 12:49:20 -0700

    Delete no conversion flags
    SYNOPSIS
            $ python main.py -vi <input-video> [-vo <output-video>]
                             -ai <input-audio> [-ao <output-audio>]
    
    DESCRIPTION
            -vo, --output-video
                    If this flag exists, `input-video` will be converted
                    into `output-video` (AVI file).
    
            -ao, --output-audio
                    Similar to `-vo`, but for audio files (converted into
                    MP3 file).
    
            -vi, --input-video
                    If `input-video` is an AVI file, no conversion.
                    Otherwise, convert it into an AVI file.
    
            -ai, --input-audio
                    Similar to `-vi`, but for audio files (convert the file
                    to an MP3 file if it is not an MP3 file).

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import argparse
 from PyQt5 import QtCore, QtGui, QtWidgets
@@ -8,35 +9,29 @@ from src.video import analyze_video
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-vi', '--input-video',
-                        required=True, help='input video filename (.rgb)', dest='v_in')
+                        required=True, help='input video filename (.rgb or .avi)', dest='v_in')
     parser.add_argument('-ai', '--input-audio',
-                        required=True, help='input audio filename (.wav)', dest='a_in')
+                        required=True, help='input audio filename (.wav or .mp3)', dest='a_in')
     parser.add_argument('-vo', '--output-video',
-                        default='output.avi', help='output video filename (.avi)', dest='v_out')
+                        default='', help='output video filename (.avi)', dest='v_out')
     parser.add_argument('-ao', '--output-audio',
-                        default='output.mp3', help='output audio filename (.mp3)', dest='a_out')
+                        default='', help='output audio filename (.mp3)', dest='a_out')
     parser.add_argument('-nc', '--no-conversion',
                         action='store_true', help='disable video & audio conversion', dest='no_conversion')
-    parser.add_argument('-nvc', '--no-video-conversion',
-                        action='store_true', help='disable video conversion', dest='no_video_conversion')
-    parser.add_argument('-nac', '--no-audio-conversion',
-                        action='store_true', help='disable audio conversion', dest='no_audio_conversion')
     args = parser.parse_args()
 
-    conversion_enabled = not args.no_conversion
-    video_conversion_enabled = not args.no_video_conversion
-    audio_conversion_enabled = not args.no_audio_conversion
+    v_name, v_ext = os.path.splitext(args.v_in)
+    a_name, a_ext = os.path.splitext(args.a_in)
 
     video_target = args.v_in
     audio_target = args.a_in
 
-    if conversion_enabled:
-        if video_conversion_enabled:
-            rgb2avi(args.v_in, args.v_out)
-            video_target = args.v_out
-        if audio_conversion_enabled:
-            wav2mp3(args.a_in, args.a_out)
-            audio_target = args.a_out
+    if args.v_out or v_ext != '.avi':
+        video_target = args.v_out if args.v_out else v_name + '.avi'
+        rgb2avi(args.v_in, video_target)
+    if args.v_out or a_ext != '.mp3':
+        audio_target = args.a_out if args.a_out else a_name + '.mp3'
+        wav2mp3(args.a_in, args.a_out)
 
     frames = analyze_video(video_target)
 
